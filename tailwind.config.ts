f305eadc442cb5fbf53f50c227623aa8e29169af
pqoqubbw <pqoqubbw@gmail.com>
2024-11-18 00:03:06 +0100

    fix tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,7 @@ export default {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './ui/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
